c8a9dc7372d3765645b967f65e4cb6db1d5abe47
Victor Zhang <victorzhangdev@gmail.com>
2022-06-20 15:33:37 -0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader></MainHeader>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"></Redirect>
+          </Route>
+          <Route path="/welcome">
+            <Welcome></Welcome>
+          </Route>
+          <Route path="/products" exact>
+            <Products></Products>
+          </Route>
+          <Route path="/products/:productID">
+            <ProductDetail></ProductDetail>
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Products.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+const Products = () => {
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Online Course</Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
